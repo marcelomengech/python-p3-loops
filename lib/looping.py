5bc85071eec083cafcc8fe68efacd875ce9536e0
Stiveys <stevomwaura@gmail.com>
2025-02-12 09:58:08 +0300

    new

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,39 @@
-#!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+    """
+    Counts down from 10 to 1 and prints "Happy New Year!"
+    using a while loop.
+    """
+    i = 10
+    while i >= 1:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+
+def square_integers(numbers):
+    """
+    Squares each element in the input list and returns the new list.
+
+    Args:
+        numbers: A list of integers.
+
+    Returns:
+        A new list containing the squared values of the input list.
+    """
+    return [number ** 2 for number in numbers]
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    """
+    Prints numbers from 1 to 100, replacing multiples of 3 with "Fizz",
+    multiples of 5 with "Buzz", and multiples of both with "FizzBuzz".
+    """
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
